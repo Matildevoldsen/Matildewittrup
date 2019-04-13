8004b8e5413e27be859abcb74a9ff228b81b98ae
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-04-13 20:49:32 +0100

    Close on click menu item using redux.

--- frontend/src/components/Navigation/Item/Item.js
+++ frontend/src/components/Navigation/Item/Item.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import './Item.css';
+import {connect} from "react-redux";
+import { showNav } from "../../../redux/actions/showNavAction.js";
 
-export default class Item extends Component{
+
+class Item extends Component{
+    constructor(props) {
+        super(props)
+
+        this.onItemClick = this.onItemClick.bind(this);
+    }
+
+    onItemClick() {
+        const condition = !this.props.Nav;
+        this.props.showNav(condition);
+    }
+
     render() {
         return(
-            <li className={this.props.classes ? this.props.classes + ' nav--item' : 'nav--item'}>
+            <li onClick={this.onItemClick} className={this.props.classes ? this.props.classes + ' nav--item' : 'nav--item'}>
                 <Link to={this.props.toPath}>{this.props.children}</Link>
             </li>
         );
     }
 }
+
+const mapStateToProps = state => ({
+    Nav: state.nav.menu,
+});
+
+export default connect(mapStateToProps, {showNav})(Item);
